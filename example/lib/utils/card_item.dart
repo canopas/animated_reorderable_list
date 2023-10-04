2b773112e7a4c6208b668f36b3ce30490beca3cc
sneha s <sneha.s@canopas.com>
2023-10-04 17:02:31 +0530

    WIP - Support grid animation

---
+++ example/lib/utils/card_item.dart
@@ -0,0 +1,25 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CardItem extends StatelessWidget {
+  final int index;
+  const CardItem({super.key,required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 80.0,
+      width: 80,
+      child: Card(
+        color: Colors.primaries[index % Colors.primaries.length],
+        child: Center(
+          child: Text(
+            (index + 1).toString(),
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/sliver_grid_motion_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class SliverGridMotionList extends StatelessWidget {
+  const SliverGridMotionList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
