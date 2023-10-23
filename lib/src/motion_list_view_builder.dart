6cfca84fff5ac2fa143e978db4581947f8a2b4da
cp-sneha-s <sneha.s@canopas.com>
2023-10-23 16:19:18 +0530

    Add parameter for customScrollView

--- lib/src/motion_list_view_builder.dart
+++ lib/src/motion_list_view_builder.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:motion_list/motion_list.dart';
 
 class MotionListViewBuilder<E extends Object> extends StatelessWidget {
@@ -22,13 +23,13 @@ class MotionListViewBuilder<E extends Object> extends StatelessWidget {
   final Axis scrollDirection;
 
   /// The duration of the animation when an item was inserted into the list.
-  final Duration? insertDuration;
+  final Duration insertDuration;
 
   /// The duration of the animation when an item was removed from the list.
-  final Duration? removeDuration;
+  final Duration removeDuration;
 
   /// The duration of the list update its position.
-  final Duration? resizeDuration;
+  final Duration resizeDuration;
 
   ///
   ///Called by the DiffUtil to decide whether two object represent the same Item.
@@ -41,34 +42,83 @@ class MotionListViewBuilder<E extends Object> extends StatelessWidget {
   ///
   final EqualityChecker? areItemsTheSame;
 
+  /// {@template flutter.widgets.scroll_view.reverse}
+  /// Whether the scroll view scrolls in the reading direction.
+  ///
+  /// For example, if the reading direction is left-to-right and
+  /// [scrollDirection] is [Axis.horizontal], then the scroll view scrolls from
+  /// left to right when [reverse] is false and from right to left when
+  /// [reverse] is true.
+  ///
+  /// Similarly, if [scrollDirection] is [Axis.vertical], then the scroll view
+  /// scrolls from top to bottom when [reverse] is false and from bottom to top
+  /// when [reverse] is true.
+  ///
+  /// Defaults to false.
+  /// {@endtemplate}
+  final bool reverse;
+
+  /// [ScrollController] to get the current scroll position.
+  final ScrollController? controller;
+
+  /// When this is true, the scroll view is scrollable even if it does not have
+  /// sufficient content to actually scroll. Otherwise, by default the user can
+  /// only scroll the view if it has sufficient content. See [physics].
+  final bool? primary;
+  final ScrollPhysics? physics;
+  final ScrollBehavior? scrollBehavior;
+  final String? restorationId;
+  final ScrollViewKeyboardDismissBehavior keyboardDismissBehavior;
+  final Clip clipBehavior;
+  final DragStartBehavior dragStartBehavior;
+
   const MotionListViewBuilder(
       {Key? key,
       required this.items,
       required this.itemBuilder,
       this.insertAnimation = AnimationType.fadeIn,
       this.removeAnimation,
-      this.insertDuration=const Duration(milliseconds: 300),
-      this.removeDuration=const Duration(milliseconds: 300),
-      this.resizeDuration=const Duration(milliseconds: 300),
-      this.scrollDirection= Axis.vertical,
-      this.areItemsTheSame})
+      this.insertDuration = const Duration(milliseconds: 300),
+      this.removeDuration = const Duration(milliseconds: 300),
+      this.resizeDuration = const Duration(milliseconds: 300),
+      this.scrollDirection = Axis.vertical,
+      this.areItemsTheSame,
+      this.reverse = false,
+      this.controller,
+      this.primary,
+      this.physics,
+      this.scrollBehavior,
+      this.restorationId,
+      this.keyboardDismissBehavior = ScrollViewKeyboardDismissBehavior.manual,
+      this.dragStartBehavior = DragStartBehavior.start,
+      this.clipBehavior = Clip.hardEdge})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(scrollDirection: scrollDirection,
-        slivers: [
-      MotionListImpl(
-        items: items,
-        itemBuilder: itemBuilder,
-        insertAnimationType: insertAnimation,
-        removeAnimationType: removeAnimation ?? insertAnimation,
-        insertDuration: insertDuration!,
-        removeDuration: removeDuration!,
-        resizeDuration: resizeDuration!,
-        areItemsTheSame: areItemsTheSame,
+    return CustomScrollView(
         scrollDirection: scrollDirection,
-      ),
-    ]);
+        reverse: reverse,
+        controller: controller,
+        primary: primary,
+        physics: physics,
+        scrollBehavior: scrollBehavior,
+        restorationId: restorationId,
+        keyboardDismissBehavior: keyboardDismissBehavior,
+        dragStartBehavior: dragStartBehavior,
+        clipBehavior: clipBehavior,
+        slivers: [
+          MotionListImpl(
+            items: items,
+            itemBuilder: itemBuilder,
+            insertAnimationType: insertAnimation,
+            removeAnimationType: removeAnimation ?? insertAnimation,
+            insertDuration: insertDuration,
+            removeDuration: removeDuration,
+            resizeDuration: resizeDuration,
+            areItemsTheSame: areItemsTheSame,
+            scrollDirection: scrollDirection,
+          ),
+        ]);
   }
 }
