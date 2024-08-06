933bf6b477a281392dc84835e511ae6f00c4d91a
cp-sneha-s <sneha.s@canopas.com>
2024-08-06 10:26:02 +0530

    Minor changes

--- lib/src/builder/motion_list_base.dart
+++ lib/src/builder/motion_list_base.dart
@@ -197,14 +197,10 @@ abstract class MotionListBaseState<
   }
 
   void calculateDiff(List oldList, List newList) {
-    // if(widget.onrReorder != null && oldList.length == newList.length) {
-    //   return;
-    // }
     // Detect removed and updated items
     for (int i = oldList.length - 1; i >= 0; i--) {
       if (newList.indexWhere((element) => isSameItem(oldList[i], element)) ==
           -1) {
-        print("item is deleted: $i");
         listKey.currentState!.removeItem(i, removeItemDuration: removeDuration);
       }
     }
@@ -212,7 +208,6 @@ abstract class MotionListBaseState<
     for (int i = 0; i < newList.length; i++) {
       if (oldList.indexWhere((element) => isSameItem(newList[i], element)) ==
           -1) {
-        print("item is added: $i");
         listKey.currentState!.insertItem(i, insertDuration: insertDuration);
       }
     }
